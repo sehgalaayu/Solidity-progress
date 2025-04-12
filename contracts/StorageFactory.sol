aa2db93c75e2f49a44e1f153264c75c7f7ff2aea
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-04-12 16:51:10 +0530

    Update StorageFactory.sol

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
-import {SimpleStorage} from ./SimpleStorage.sol;
+import { SimpleStorage } from "./SimpleStorage.sol";
 
-contract StorageFactory{
-    SimpleStorage public simpleStorage;
+contract StorageFactory {
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
-    function createSimpleStorageContract() public{
-        simpleStorage = new SimpleStorage();
-
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(simpleStorageContract);
     }
-    
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
 }
