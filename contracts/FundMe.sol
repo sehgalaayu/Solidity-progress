4e076069bad3b9f01fe4a381944aad096c45ef6e
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-04-19 18:29:39 +0530

    Update FundMe.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -56,17 +56,7 @@ contract FundMe {
         (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
-    // Explainer from: https://solidity-by-example.org/fallback/
-    // Ether is sent to contract
-    //      is msg.data empty?
-    //          /   \
-    //         yes  no
-    //         /     \
-    //    receive()?  fallback()
-    //     /   \
-    //   yes   no
-    //  /        \
-    //receive()  fallback()
+
 
     fallback() external payable {
         fund();
@@ -77,11 +67,4 @@ contract FundMe {
     }
 }
 
-// Concepts we didn't cover yet (will cover in later sections)
-// 1. Enum
-// 2. Events
-// 3. Try / Catch
-// 4. Function Selector
-// 5. abi.encode / decode
-// 6. Hash with keccak256
-// 7. Yul / Assembly
+
