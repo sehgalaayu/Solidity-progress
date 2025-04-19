302e9c9d1124ff1285ee3d4fc7e89ac709ed40d8
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-04-19 17:40:02 +0530

    Update PriceConvertor.sol

--- contracts/PriceConvertor.sol
+++ contracts/PriceConvertor.sol
@@ -1,21 +1,29 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
-pragma solidity ^0.8.19;
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol"; 
 
-library PriceConvertor{
- address public constant PRICE_FEED_ADDRESS = 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43;
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(PRICE_FEED_ADDRESS);
-        (, int256 answer,,,) = priceFeed.latestRoundData();
-        return uint256(answer * 1e10); // Convert price from 8 to 18 decimals
+
+library PriceConverter {
+   
+    function getPrice() internal view returns (uint256) {
+        // Sepolia ETH / USD Address
+        // https://docs.chain.link/data-feeds/price-feeds/addresses
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        // ETH/USD rate in 18 digit
+        return uint256(answer * 10000000000);
     }
 
-    // Convert ETH amount to USD equivalent
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+    // 1000000000
+    function getConversionRate(
+        uint256 ethAmount
+    ) internal view returns (uint256) {
         uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
         return ethAmountInUsd;
     }
-
 }
