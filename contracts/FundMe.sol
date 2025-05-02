284760b02f33eda1cfa382c99e03aab56c7455aa
sehgalaayu <119155282+sehgalaayu@users.noreply.github.com>
2025-05-02 23:26:21 +0530

    Update FundMe.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -13,8 +13,7 @@ contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
+    address public i_owner;
     uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
 
     constructor() {
